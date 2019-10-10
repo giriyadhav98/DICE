d36e79cfbff9f8bb60510b06787553f293eaf375
Giriyadhav98 <giridharyadav98@gmail.com>
2019-10-10 13:30:02 +0530

    Initial commit

---
+++ dice.py
@@ -0,0 +1,16 @@
+import random
+import time
+
+roll_again = "yes"
+
+while roll_again == "yes" or roll_again == 'y' :
+    print ("\n rooling the dice...")
+    time.sleep (1)
+
+    dice = random.randint (1, 6)
+
+    print ("the values are:")
+    print ("Dice =", dice)
+
+    roll_again = input ("\n Roll the dice again? (Y/N) ")
+
